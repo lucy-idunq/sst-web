f8067980aa10ac961847f828d0ede72065c25ebb
lucylyida <lucylyida188@gmail.com>
2020-12-01 11:56:10 +0630

    lucy pull

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import Navbar from './Navbar';
+import Navbar from './Navbar';
 import SideBar from './SideBar';
 
 const NavigationBar = props => {
@@ -13,15 +13,15 @@ const NavigationBar = props => {
     return (
         <div className="">
             <div className="d-flex flex-row">
-                <div className={``} style={{minWidth:'300px'}}>
+                <div className={``} style={{ minWidth: '300px' }}>
                     <SideBar sidebarOpen={sidebarOpen} />
                 </div>
-                {/* <div className='flex-grow-1'>
+                <div className='flex-grow-1'>
                     <Navbar Open={Open} />
-                </div> */}
+                </div>
             </div>
         </div>
-      
+
 
     )
 }
