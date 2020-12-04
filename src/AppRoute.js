992030a1806cab06adc4831de61cf93a4a7d2968
lucylyida <lucylyida188@gmail.com>
2020-12-04 10:06:17 +0630

    sidebar item justify

---
+++ src/AppRoute.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import { Route, Switch, withRouter } from "react-router-dom";
+
+// import Dashboard from './Components/Dashboard/Dashboard'
+// import Items from './Components/Items/Items'
+// import NavContainer from './Components/Navigation/NavContainer'
+// import Register from './Components/Login/Register'
+// import Login from './Components/Login/Login'
+// import Orders from './Components/Order/Orders'
+
+import Dashboard from './Components/Dashboard/Dashboard'
+import Items from './Components/Items/Items'
+import Orders from './Components/Order/Orders'
+import Register from './Components/Login/Register'
+import Login from './Components/Login/Login'
+import NavContainer from './Components/Navigation/NavContainer'
+
+
+const AppRoute = props => {
+    const { history } = props
+    const routeName = ['/login', '/register',]
+    return (
+        
+        <div >
+            {!routeName.includes(history.location.pathname) && <NavContainer />}
+            <Switch>
+                <Route path="/items" component={Items} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/orders" component={Orders} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        </div>
+
+    )
+}
+export default withRouter(AppRoute);
--- src/Components/Navigation/NavContainer.js
+++ src/Components/Navigation/NavContainer.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import SideBar from './SideBar';
 import NavBar from './NavBar';
 
-import AppRoute from './AppRoute.js'
+
 
 const NavContainer = props => {
     const [sidebarOpen, setsidebarOpen] = useState(false)
@@ -12,14 +12,13 @@ const NavContainer = props => {
     return (
         <div className="container-fluid p-0">
             <div className="row ">
-                <div className={`${sidebarOpen ? 'd-none' : 'col-2 col-lg-1'} p-0`}>
+                <div className={`${sidebarOpen ? 'd-none' : 'col-2 col-lg-1'} p-0 `}>
                     <SideBar />
                 </div>
                 <div className={`${sidebarOpen ? 'col-12' : 'col-10 col-lg-11'} p-0`}>
                     <NavBar setsidebarOpen={setsidebarOpen} sidebarOpen={sidebarOpen} />
-                    <AppRoute/>
                 </div>
-                
+
 
             </div>
         </div>
